a577e66648e86d13efb3665423c6db492ea839af
ricardo <ricardogp1993@gmail.com>
2019-12-09 13:47:55 -0300

    Basic authentication navigation implementation.

--- app/src/main/java/com/rgp/ticketapp/LoginFragment.kt
+++ app/src/main/java/com/rgp/ticketapp/LoginFragment.kt
@@ -2,10 +2,12 @@ package com.rgp.ticketapp
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class LoginFragment : Fragment() {
 
@@ -16,5 +18,18 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //callback to loginButton
+        view.findViewById<Button>(R.id.loginButton).setOnClickListener {
+            findNavController().navigate(R.id.action_global_ticketsFragment)
+        }
+
+        //callback to registerButton
+        view.findViewById<Button>(R.id.registerButton).setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
 
 }
--- app/src/main/java/com/rgp/ticketapp/RegisterFragment.kt
+++ app/src/main/java/com/rgp/ticketapp/RegisterFragment.kt
@@ -2,10 +2,12 @@ package com.rgp.ticketapp
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class RegisterFragment : Fragment() {
 
@@ -14,6 +16,15 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //callback to registerButton
+        view.findViewById<Button>(R.id.registerButton).setOnClickListener {
+            findNavController().navigate(R.id.action_global_ticketsFragment)
+        }
     }
 
 

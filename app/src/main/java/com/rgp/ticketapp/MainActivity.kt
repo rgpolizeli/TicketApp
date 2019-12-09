e480a11b27d1d0e597c219975a0296f468364871
ricardo <ricardogp1993@gmail.com>
2019-12-09 13:54:49 -0300

    App toolbar configuration.

--- app/src/main/java/com/rgp/ticketapp/MainActivity.kt
+++ app/src/main/java/com/rgp/ticketapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.rgp.ticketapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configToolbar()
+    }
+
+    private fun configToolbar() {
+        setSupportActionBar(findViewById(R.id.mainToolbar))
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.mainToolbar)
+            .setupWithNavController(navController, appBarConfiguration)
     }
 }
